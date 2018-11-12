637ac130852c4625de9dfb32fae25cb423275d70
Jd <jddavenport46@gmail.com>
2018-11-12 13:13:06 -0700

    drop down

--- Project1/Controllers/MissionsController.cs
+++ Project1/Controllers/MissionsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,35 @@ namespace Project1.Controllers
     public class MissionsController : Controller
     {
         // GET: Missions
+        
+        static public List<Product> ListOfProducts = new List<Product>();
+        
         public ActionResult Index()
         {
+            ListOfProducts.Add(new Product("Straw 1", "JD", "Provo", "English"));
+            ListOfProducts.Add(new Product("Straw 2", "Me", "SLC", "English"));
+            ListOfProducts.Add(new Product("Straw 3", "You", "Sandy", "English"));
             return View();
+        }
+        public ActionResult Display(int? num)
+        {
+            Product Send2View;
+            if (num==1)
+            {
+                Send2View = ListOfProducts[0];
+                //Send2View is option 1
+            }
+            else if (num==2)
+            {
+                Send2View = ListOfProducts[1];
+                //Send2View is option 2
+            }
+            else
+            {
+                Send2View = ListOfProducts[2];
+                //Send2Veiw is option 3
+            }
+            return View(Send2View);
         }
     }
 }
---
+++ Project1/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace Project1.Models
+{
+    public class Product
+    {
+        public string name { get; set; }
+        public string manfacture { get; set; }
+        public string location { get; set; }
+        public string language { get; set; }
+        public Image Picture { get; set; }
+
+        public Product(string name, string manfacture, string location, string language, Image picture)
+        {
+            this.name = name;
+            this.manfacture = manfacture;
+            this.location = location;
+            this.language = language;
+            Picture = picture;
+        }
+
+        public Product(string name, string manfacture, string location, string language)
+        {
+            this.name = name;
+            this.manfacture = manfacture;
+            this.location = location;
+            this.language = language;
+        }
+    }
+}
