9062484573efea665d95d690135a1f01d7d65e25
Connor McLeod <connorraymcleod@gmail.com>
2018-11-12 14:15:38 -0700

    connors_branch

---
+++ Project1/Models/Contact.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project1.Models
+{
+    public class Contact
+    {
+        [DisplayName("First Name")]
+        public string contactFName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string contactLName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DisplayName("Email Address")]
+        public string contactEmailAddress { get; set; }
+
+
+        [Required]
+        [EmailAddress]
+        [Compare("contactEmailAddress")]
+        [DisplayName("Verify Email Address")]
+        public string contactVerifyEmailAddress { get; set; }
+
+        [DisplayName("Subject")]
+        public string ContactSubject { get; set; }
+
+        [Required]
+        [DisplayName("Message")]
+        public string contactBody { get; set; }
+
+    }
+}
