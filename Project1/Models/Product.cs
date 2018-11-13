dc975c34bd426802dfd4db0f17fb39a00f57813e
Hoyong Jung <jhoyong57@gmail.com>
2018-11-12 21:59:49 -0700

    Little change

--- Project1/Models/Product.cs
+++ Project1/Models/Product.cs
@@ -11,27 +11,27 @@ namespace Project1.Models
 {
     public class Product
     {
-        public string name { get; set; }
-        public string manfacture { get; set; }
-        public string location { get; set; }
-        public string language { get; set; }
+        public string Name { get; set; }
+        public string Manfacture { get; set; }
+        public string Location { get; set; }
+        public string Language { get; set; }
         public Image Picture { get; set; }
 
         public Product(string name, string manfacture, string location, string language, Image picture)
         {
-            this.name = name;
-            this.manfacture = manfacture;
-            this.location = location;
-            this.language = language;
+            this.Name = name;
+            this.Manfacture = manfacture;
+            this.Location = location;
+            this.Language = language;
             Picture = picture;
         }
 
         public Product(string name, string manfacture, string location, string language)
         {
-            this.name = name;
-            this.manfacture = manfacture;
-            this.location = location;
-            this.language = language;
+            this.Name = name;
+            this.Manfacture = manfacture;
+            this.Location = location;
+            this.Language = language;
         }
 
         public Product()
