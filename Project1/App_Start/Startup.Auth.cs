5cd18bdcb8169b2a6a5c44cc09bd9498e9e1cbde
Hoyong Jung <jhoyong57@gmail.com>
2018-12-12 16:08:35 -0700

    add lots of NuGits

---
+++ Project1/App_Start/Startup.Auth.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project1.App_Start
+{
+    public class Startup
+    {
+    }
+
+    public void ConfigureAuth(IAppBuilder app)
+    {
+    }
+
+}
