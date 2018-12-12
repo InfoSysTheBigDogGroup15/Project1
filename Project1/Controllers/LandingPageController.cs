2efef80b16b60ce5208364c46026a6968bfb53fd
Grant Valentine <grantsvalentine@gmail.com>
2018-12-11 19:29:23 -0700

    Logins

--- Project1/Controllers/LandingPageController.cs
+++ Project1/Controllers/LandingPageController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Project1.Controllers
 {
     public class LandingPageController : Controller
     {
         // GET: LandingPage
+        [Authorize]
         public ActionResult Index()
         {
             return View();
@@ -17,6 +19,30 @@ namespace Project1.Controllers
         public ActionResult AboutUs()
         {
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection form, bool rememberMe = false)
+        {
+            String email = form["Email address"].ToString();
+            String password = form["Password"].ToString();
+
+            if (string.Equals(email, "greg@test.com") && (string.Equals(password, "Greg")))
+            {
+                FormsAuthentication.SetAuthCookie(email, rememberMe);
+
+                return RedirectToAction("Index", "LandingPage");
+
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
